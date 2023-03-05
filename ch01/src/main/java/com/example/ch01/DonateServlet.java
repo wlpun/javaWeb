065fe8bf3c1d005dd12bb92de0d46938fba2c60e
aliyun5607855259 <1662889612@qq.com>
2023-03-05 11:55:43 +0800

    1

---
+++ ch01/src/main/java/com/example/ch01/DonateServlet.java
@@ -0,0 +1,29 @@
+package com.example.ch01;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DonateServlet", value = "/DonateServlet")
+public class DonateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name = request.getParameter("name");
+        String bloodtype = request.getParameter("bloodtype");
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h3>无偿献血是无私奉献、救死扶伤的崇高行为。</h3>");
+        out.println("<p>感谢您！"+bloodtype+"型血的"+name+"朋友。</p>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
