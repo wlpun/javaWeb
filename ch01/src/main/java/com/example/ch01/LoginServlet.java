c530a2db26f824d2a1853405743c6d374e2f1989
aliyun5607855259 <1662889612@qq.com>
2023-03-27 16:23:39 +0800

    4

---
+++ ch01/src/main/java/com/example/ch01/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.example.ch01;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.Random;
+
+@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name = request.getParameter("username");
+        String pwd = request.getParameter("pwd");
+        if(name.equals("21141132") && pwd.equals("123")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", name);
+            int num = new Random().nextInt(101);
+            session.setAttribute("num", num);
+            response.sendRedirect(request.getContextPath()+"/lucky.jsp");
+        }
+        else response.sendRedirect(request.getContextPath() + "/login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
---
+++ ch01/src/main/webapp/lucky.jsp
@@ -0,0 +1,21 @@
+<%@ page import="java.io.PrintWriter" %><%--
+  Created by IntelliJ IDEA.
+  User: 16628
+  Date: 2023-03-27
+  Time: 16:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <%
+        HttpSession session1 = request.getSession();
+        Object username = session1.getAttribute("username");
+        Object num = session1.getAttribute("num");
+    %>
+    <%= username + "\n" + num%>
+</body>
+</html>
